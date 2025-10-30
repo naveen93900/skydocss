8927640949c628febf23682868bd4e65e254b5a7
Venkata Naveen Mallapuram <venkatanaveen@STIblr.com>
2025-10-30 19:54:01 +0530

    Add Maven wrapper files

---
+++ src/main/java/com/SkyDoc/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.SkyDoc.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+    public String home() {
+        return "✅ SkyDocs Backend is Running Successfully!";
+    }
+}
